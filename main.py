e5bd67fb808b3c8073a6b6e321eda3e8016ccef0
Tinyy <73321003+aka-tinyy@users.noreply.github.com>
2022-10-10 22:16:51 -0400

    Updated main.py
    Added local caching that updated every 20 minutes from the api

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask import jsonify, make_response, request
 import pymongo
+import threading
 
 connection = pymongo.MongoClient("")
 db = connection[""]
 collection = db[""]
+freegameslist = []
 
 app = flask.Flask(__name__)
 limiter = Limiter(
@@ -15,31 +17,46 @@ limiter = Limiter(
     default_limits=["60 per hour"])
 
 
+def pull_newgames():
+    threading.Timer(1200.0, pull_newgames).start()
+    freegameslist.clear()
+    for game in collection.find({}, {"_id": 0}):  # Pulls all games from the database
+        freegameslist.append(game)
+
+pull_newgames()
+
+
+@app.route('/', methods=['GET'])
+@limiter.limit("1 per minute")
+def home():
+    return jsonify({"message": "Welcome to free games bot api! For more info, visit https://docs.freegamesbot.xyz"})
+
+
 @app.route('/ping', methods=['GET'])
 @limiter.limit("1 per minute")
 def ping():
     return jsonify({"status": "API is alive"})
 
 
-@app.route('/freegames', methods=['GET'], defaults={'type': 'all'})
+@app.route('/freegames', methods=['GET'])
 @limiter.limit("1 per minute")
 def freegames():
     type = request.args.get('type')
     lst = []
-    x = collection.find({}, {"_id": 0})
     if type == "all":
-        for i in x:
+        for i in freegameslist:
             lst.append(i)
     elif type == "epicgames":
-        for i in x:
+        for i in freegameslist:
             if i["Platform"] == "Epic Games":
                 lst.append(i)
     elif type == "gog":
-        for i in x:
+        for i in freegameslist:
             if i["Platform"] == "GOG":
                 lst.append(i)
-    elif type is None:
-        return make_response(jsonify({"error": "No type specified"}), 400)
+    elif type is (None or ""):
+        for i in freegameslist:
+            lst.append(i)
     return jsonify({'result' : lst}, 200)
 
 
